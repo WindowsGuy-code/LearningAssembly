b8523661c94384000fe137bd4bbeb62cee2b3374
Guy <paike.cool@gmail.com>
2025-09-23 09:39:57 +0200

    Update test.asm

--- test.asm
+++ test.asm
@@ -13,6 +13,7 @@ extern ExAllocatePool
 %define e_magic 0x5A4D
 %define SIGNATURE_INCORRECT 0x0000401
 %define nt_signature 0x50450000
+%define STATUS_FAILED 0x50450320
 
 section .data:
 LoadedMSG db '[PatternDriver] Loaded', 10, 0
@@ -43,23 +44,35 @@ specialExit:
   add rsp, 40
   ret
 
-ReturnResults:
+ReturnResults: ; rdi: ptr section rcx: addr
   mov r10, dword ptr [rdi + 0x0C]
   lea rax, [rcx + r10]
   mov rdi, dword ptr [rdi + 0x08]
   add rsp, 40
   ret
 
-SectionLoop: ; rcx addr, rdi: ptr Section r8: numSections r9: baseOfSection
-  movzx r9, 0x40
-  mov rdx, r8
-  mov rax, r9
-  mul rax
+SectionLoop: ; rcx addr, rdi: ptr Section r8: numSections r9: Name
+  mov r10, rcx
+  mov r11, rdi
 
-  
+  xor rax, rax
+  movzx rcx, byte ptr [r11]
+  movzx rdi, byte ptr [r9]
+  call strstr
+  test rax, rax
+  mov rdi, r11
+  mov rcx, r10
+  jz ReturnResult
+
+  cmp r9, r12
+  mov rax, STATUS_FAILED
+  je specialExit
+  inc r12
+  lea rcx, [rcx + 0x40]
+  jmp SectionLoop
 
 
-FindSection: ;rcx: addr rdi: ptr Section
+FindSection: ;rcx: addr rdi: ptr Section(Name)
   sub rsp, 40
   movzx r10, byte ptr [rcx]
   movzx r11, e_magic
@@ -76,25 +89,25 @@ FindSection: ;rcx: addr rdi: ptr Section
   je specialExit
 
   push rcx
-  mov rcx, r12
+  mov rcx, [r12]
   call IMAGE_FIRST_SECTION
   push rax
-  push rdi
+  push rdi ; SectionName
   movzx rcx, byte ptr [rax]
-  movzx rdi, byte ptr [rdi]
+  movzx rdi, byte ptr [rdi] ; SectionName
   call strstr
   test rax, rax
-  pop rdi
-  pop rax
+  pop r9 ; sectionName
+  pop rax ; ptr Section
+  mov rdi, rax
+  pop rcx ; addr
   jz ReturnResults
 
   
-  lea r8, [r13 + 0x06]
+  lea r8, [r12 + 0x06]
   movzx r8, dword ptr [r8]
-  mov r9, rdi
-  pop rdi
 
-  jmp SectionLoop
+  jmp SectionLoop ; rcx addr, rdi: ptr Section r8: numSections r9: Name
   
 
 ; Helper2 [Info gathering]
